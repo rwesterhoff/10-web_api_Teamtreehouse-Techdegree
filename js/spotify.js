289b5627096a7df8d3ba1cd7b49c8f6984099dfd
Robin Westerhoff <rwesterhoff@me.com>
2016-11-09 22:00:50 +0100

    Improving first step

--- js/spotify.js
+++ js/spotify.js
@@ -1,47 +1,38 @@
- var $searchQuery = $('#query').val();
-
- function fetchTracks(albumId, callback) {
-     $.ajax({
-         url: 'https://api.spotify.com/v1/albums/' + albumId,
-         success: function(response) {
-             callback(response);
-         }
-     });
- };
-
-
- function displayResults(data) {
-     var resultHTML;
-     $.each(array, function(i, item) {
-         resultHTML += '<li class="gallery-item">';
-         resultHTML += '<a href="">';
-         resultHTML += '<img src="" alt=">';
-         resultHTML += '</a>';
-         resultHTML += '</li>';
-     });
-     $gallery.html(resultHTML);
- };
-
- function searchAlbums(query) {
-     $.ajax({
-         url: 'https://api.spotify.com/v1/search',
-         data: {
-             q: query,
-             type: 'album'
-         },
-         success: function(response) {
-             var resultHTML;
-             $.each(array, function(i, item) {
-                 resultHTML += '<li class="gallery-item">';
-                 resultHTML += '<a href="">';
-                 resultHTML += '<img src="" alt=">';
-                 resultHTML += '</a>';
-                 resultHTML += '</li>';
-             });
-             $gallery.html(resultHTML);
-         }
-     });
- };
+ var $searchQuery = $('#query').val(),
+     /*fetchTracks = function(albumId, callback) {
+         $.ajax({
+             url: 'https://api.spotify.com/v1/albums/' + albumId,
+             success: function(response) {
+                 callback(response);
+             }
+         });
+     },
+     displayResults = function(data) {
+         var resultHTML;
+         $.each(array, function(i, item) {
+             resultHTML += '<li class="gallery-item">';
+             resultHTML += '<a href="">';
+             resultHTML += '<img src="" alt=">';
+             resultHTML += '</a>';
+             resultHTML += '</li>';
+         });
+         $gallery.html(resultHTML);
+     },*/
+     searchAlbums = function(query) {
+         $.ajax({
+             url: 'https://api.spotify.com/v1/search',
+             data: {
+                 q: query,
+                 type: 'album'
+             }
+         }).done(function() {
+            alert('Success');
+         }).fail(function() {
+            alert('Failed');
+         }).always(function(){
+            alert('Complete');
+         });
+     };
 
 
 
