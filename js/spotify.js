0e33aa3d799e7b5b4acdfa547c3dc6264716abef
Robin Westerhoff <rwesterhoff@me.com>
2016-11-10 20:24:13 +0100

    Styling the overlay, almost there.

--- js/spotify.js
+++ js/spotify.js
@@ -9,6 +9,30 @@ var $input = $('#query');
     SEARCH
 \* --------------------------------------------------------------------------- */
 
+// Show the details in an overlay
+function showDetails(id) {
+    var albumId = id;
+    // Get data 
+    $.ajax({
+        url: 'https://api.spotify.com/v1/albums/' + albumId,
+        success: function(response) {
+            var artistName = response.artists[0].name,
+                albumName = response.name,
+                albumImgUrl = response.images[0].url,
+                albumReleased = response.release_date,
+                tracks = response.tracks.items,
+                trackList = [];
+
+            $.each(tracks, function(i, track) {
+                var trackName = track.name;
+                // Add each track track list
+                trackList += trackName;
+            });
+            console.log( /*albumId, artistName, albumName, albumImgUrl, albumReleased, */ trackList);
+        }
+    });
+}
+
 // Search albums based on keyword
 function searchAlbums() {
     var searchQuery = $input.val();
@@ -24,20 +48,27 @@ function searchAlbums() {
             var imageGallery = $('#image-gallery'),
                 albums = response.albums.items,
                 resultHTML = '';
+            console.log(albums);
 
             $.each(albums, function(i, album) {
-                var albumName = album.name,
-                    albumImgUrl = album.images[0].url,
+                var albumId = album.id,
+                    albumName = album.name,
                     albumThumbUrl = album.images[1].url;
                 // Add each album HTML to the result list
                 resultHTML += '<li class="gallery-item">';
-                resultHTML += '<a href="' + albumImgUrl + '">';
-                resultHTML += '<img src="' + albumThumbUrl + '" alt="' + albumName + '">';
+                resultHTML += '<a href="">';
+                resultHTML += '<img id="' + albumId + '" src="' + albumThumbUrl + '" alt="' + albumName + '">';
                 resultHTML += '</a>';
                 resultHTML += '</li>';
             });
             // Inject the HTML into the result list
             imageGallery.html(resultHTML);
+            //On click of thumbnail
+            $("a").click(function(event) {
+                event.preventDefault();
+                var requestedAlbumId = $(this).find('img').attr('id');
+                showDetails(requestedAlbumId);
+            });
         }
     });
 }
@@ -52,10 +83,6 @@ $($input).keyup(function() {
     GALLERY
 \* --------------------------------------------------------------------------- */
 
-//On click of thumbnail
-$("a").click(function(event) {
-    event.preventDefault();
-});
 
 // If a thumbnail is clicked
 // A lightbox shows up
