433f5c2925ca73c27f4d27acdf85b0908e9213a9
Robin Westerhoff <rwesterhoff@me.com>
2016-11-10 13:23:58 +0100

    Getting a 'Succes' message

--- js/spotify.js
+++ js/spotify.js
@@ -1,58 +1,29 @@
- var $searchQuery = $('#query').val(),
-     /*fetchTracks = function(albumId, callback) {
-         $.ajax({
-             url: 'https://api.spotify.com/v1/albums/' + albumId,
-             success: function(response) {
-                 callback(response);
-             }
-         });
-     },*/
+ function searchAlbums() {
+     var $searchQuery = $('#query').val();
 
-/*CHECK THIS
-https://developer.spotify.com/web-api/object-model/#artist-object-full
-*/
+     // Get data 
+     $.ajax({
+         url: 'https://api.spotify.com/v1/search',
+         data: {
+             q: $searchQuery,
+             type: 'album'
+         }
+     }).done(function(response) {
+         alert('Success');
+         console.log(response);
+     }).fail(function() {
+         alert('Failed');
+     }).always(function() {
+         console.log(query);
+         alert('Complete');
+     });
+ };
 
-
-     displayResults = function(data) {
-         var resultHTML,
-             albums = data.albums;
-         $.each(array, function(i, album) {
-             resultHTML += '<li class="gallery-item">';
-             resultHTML += '<a href="">';
-             // resultHTML += '<img src="" alt=">';
-             resultHTML += album.name;
-             resultHTML += '</a>';
-             resultHTML += '</li>';
-         });
-         $gallery.html(resultHTML);
-     },
-     searchAlbums = function(query) {
-         $.ajax({
-             url: 'https://api.spotify.com/v1/search',
-             data: {
-                 artists: {
-                     name: query
-                 },
-                 album_type: 'album'
-             }
-         }).done(function(response) {
-             alert('Success');
-             displayResults();
-         }).fail(function() {
-             alert('Failed');
-         }).always(function() {
-             alert('Complete');
-         });
-     };
-
-
-
+ // If keyword is entered in the search field
  $('form').submit(function(e) {
      e.preventDefault();
-
-     // If keyword is entered in the search field
-     searchAlbums($searchQuery);
-     // Get data 
+     // Search for keyword
+     searchAlbums();
      // Display results
      // If a thumbnail is clicked
      // A lightbox shows up
