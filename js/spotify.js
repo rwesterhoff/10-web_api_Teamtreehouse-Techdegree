9eeec9350f2640e9c16fa12529d252250666ce80
Robin Westerhoff <rwesterhoff@me.com>
2016-11-10 17:09:13 +0100

    Trying to preventDefault interaction on clicked anchor, doesn't seem to work yet.

--- js/spotify.js
+++ js/spotify.js
@@ -1,51 +1,8 @@
 /* --------------------------------------------------------------------------- *\
-    GLOBAL VARIABLES
+    VARIABLES
 \* --------------------------------------------------------------------------- */
 
-
-//Gallery
-// var $selectedItem = '.selected';
-// var $classSelected = 'selected';
-var $galleryItem = '.gallery-item';
-// var $gallery = '#image-gallery';
-// var $totalItems = $('li').size();
-// var $currentItemIndex;
-// var $prevItemIndex;
-// var $nextItemIndex;
-// var $firstItemIndex = 0;
-// var $lastItemIndex = $totalItems - 1;
-// var $hoverTile;
-
-//Links
-// var $activeLink;
-// var $activeCaption;
-
-//Overlay
-// var $overlay = '#js-image-overlay';
-// var $carousel = '#js-carousel';
-// var $slidesWrapper = "#js-carousel-slides";
-// var $currentSlide = '.current-slide';
-
-//Injected HTML (for building a fresh overlay each time an item is clicked)
-// var $overlayHtml = '<div id="js-image-overlay"></div>';
-// var $closeHtml = '<button id="js-close-overlay" class="close-overlay">Close overlay</button>';
-// var $carouselHtml = '<div id="js-carousel" class="carousel"></div>';
-// var $slidesWrapperHtml = '<div id="js-carousel-slides" class="carousel-slides">';
-// var $slideHtml = '<div class="carousel-slide current-slide"></div>';
-// var $imageHtml = '<img>';
-// var $iframeHtml = '<div class="videoWrapper"><iframe frameborder="0"></iframe></div>';
-// var $captionHtml = '<figcaption></figcaption>';
-// var $prevButtonHtml = '<button id="previous-slide" class="carousel-control left-control">Previous</button>';
-// var $nextButtonHtml = '<button id="next-slide" class="carousel-control right-control">Next</button>';
-
-//Controls
-// var $prevButton = '#previous-slide';
-// var $nextButton = '#next-slide';
-// var $closeButton = '#js-close-overlay';
-
-//Transitions
-// var $singleDuration = 200;
-// var $doubleDuration = 400;
+var $input = $('#query');
 
 
 /* --------------------------------------------------------------------------- *\
@@ -54,7 +11,7 @@ var $galleryItem = '.gallery-item';
 
 // Search albums based on keyword
 function searchAlbums() {
-    var searchQuery = $('#query').val();
+    var searchQuery = $input.val();
 
     // Get data 
     $.ajax({
@@ -84,7 +41,6 @@ function searchAlbums() {
         }
     });
 }
-var $input = $('#query');
 
 // While typing anything in the search field
 $($input).keyup(function() {
@@ -92,138 +48,14 @@ $($input).keyup(function() {
     searchAlbums();
 });
 
-
-/* --------------------------------------------------------------------------- *\
-    OVERLAY
-\* --------------------------------------------------------------------------- */
-
-//Get and set all data for the carousel 
-//based on the click event
-function showOverlay(selectItem) {
-
-    // function injectOverlay() {
-    //     $('body').prepend($overlayHtml);
-    //     $($overlay).hide();
-    //     $($overlay).append($closeHtml + $carouselHtml);
-    //     $($carousel).append($slidesWrapperHtml);
-    //     $($slidesWrapper).append($slideHtml + $prevButtonHtml + $nextButtonHtml);
-    // }
-
-    // //Get data from selected item (getting stuff)
-    // function getSlideData(selectItem) {
-    //     $activeLink = selectItem.find('a').attr('href');
-    //     $activeCaption = selectItem.find('img').attr('title');
-    // }
-
-    // //Control the entire thing
-    // function carouselControl() {
-
-
-    //     //Get new data en load slide
-    //     function getNewSlide(selectItem) {
-
-    //         function animateSlide() {
-    //             $($currentSlide).fadeOut($singleDuration).fadeIn($singleDuration);
-    //         }
-
-    //         highlightSelected(selectItem);
-    //         getSlideData(selectItem);
-    //         animateSlide();
-    //         setTimeout(loadCarouselSlide, $singleDuration);
-    //         setIndex();
-    //     }
-
-    //     function setPrevSlide() {
-    //         if ($currentItemIndex > $firstItemIndex) {
-    //             showArrows();
-    //             $currentItemIndex -= 1;
-    //             getNewSlide($($selectedItem).prev());
-    //         }
-    //         if ($currentItemIndex === $firstItemIndex) {
-    //             hidePrevArrow();
-    //         }
-    //     }
-
-    //     function setNextSlide() {
-    //         if ($currentItemIndex < $lastItemIndex) {
-    //             showArrows();
-    //             $currentItemIndex += 1;
-    //             getNewSlide($($selectedItem).next());
-    //         }
-    //         if ($currentItemIndex === $lastItemIndex) {
-    //             hideNextArrow();
-    //         }
-    //     }
-
-    //     //Hiding the overlay
-    //     function hideOverlay() {
-    //         $($overlay).fadeOut($doubleDuration);
-
-    //         function removeOverlay() {
-    //             $($overlay).remove();
-    //         }
-
-    //         setTimeout(removeOverlay, $doubleDuration);
-    //         $(document).off('keydown');
-
-    //     }
-
-    //     //On click left + right arrows
-    //     $($prevButton).click(function() {
-    //         setPrevSlide();
-    //     });
-    //     $($nextButton).click(function() {
-    //         setNextSlide();
-    //     });
-    //     $($closeButton).click(function() {
-    //         hideOverlay();
-    //     });
-
-    //     //On keypress
-    //     $(document).on('keydown', function(event) {
-    //         switch (event.which) {
-    //             case 37: // Left arrow
-    //                 setPrevSlide();
-    //                 break;
-
-    //             case 39: // Right arrow
-    //                 setNextSlide();
-    //                 break;
-
-    //             case 27: // 'Esc'
-    //                 hideOverlay();
-    //                 break;
-
-    //             default:
-    //                 return; // exit this handler for other keys
-    //         }
-    //     });
-    // }
-
-    // injectOverlay();
-    // highlightSelected(selectItem);
-    // getSlideData(selectItem);
-    // loadCarouselSlide();
-    // $($overlay).fadeIn($doubleDuration);
-    // carouselControl();
-}
-
-
 /* --------------------------------------------------------------------------- *\
     GALLERY
 \* --------------------------------------------------------------------------- */
 
 //On click of thumbnail
-$($galleryItem).click(function(event) {
-
-    //Define index of clicked item
-    $currentItemIndex = $($(this)).index();
+$("a").click(function(event) {
     event.preventDefault();
-
-    showOverlay($(this));
-    setIndex();
 });
-
 
 // If a thumbnail is clicked
 // A lightbox shows up
