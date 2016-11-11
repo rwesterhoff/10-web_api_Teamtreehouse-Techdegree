a2df5d01304e00cdfac7847d4964ffbb2b3a0377
Robin Westerhoff <rwesterhoff@me.com>
2016-11-11 10:06:50 +0100

    Working overlay, still need fixes on height of this and the main container.

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,12 @@
 /* --------------------------------------------------------------------------- *\
-    VARIABLES
+    GLOBAL VARIABLES
 \* --------------------------------------------------------------------------- */
 
 var $input = $('#query');
 
 
 /* --------------------------------------------------------------------------- *\
-    SEARCH
+    OVERLAY
 \* --------------------------------------------------------------------------- */
 
 // Show the details in an overlay
@@ -21,22 +21,66 @@ function showDetails(id) {
                 albumImgUrl = response.images[0].url,
                 albumReleased = response.release_date,
                 tracks = response.tracks.items,
-                trackList = [];
+                overlayHTML = '',
+                overlay = '#js-image-overlay',
+                closeButton = '#js-close-overlay',
+                showOverlay = function() {
+                    // Opening HTML and fill with artist and album data
+                    overlayHTML += '<div id="js-image-overlay">';
+                    overlayHTML += '<div id="js-overlay-wrapper">';
+                    overlayHTML += '<button id="js-close-overlay" class="close-overlay">Close overlay</button>';
+                    overlayHTML += '<img src="' + albumImgUrl + '" alt="" class="album-artwork">';
+                    overlayHTML += '<section id="js-data-wrapper">';
+                    overlayHTML += '<h1>' + albumName + '</h1>';
+                    overlayHTML += '<p><span class="visually-hidden">Artist: </span>' + artistName + '</p>';
+                    overlayHTML += '<p class="meta">Released on ';
+                    overlayHTML += '<time datetime="' + albumReleased + '">' + albumReleased + '</time>';
+                    overlayHTML += '</p>';
+                    overlayHTML += '<ul class="play-list">';
+                    // Populate track list
+                    $.each(tracks, function(i, track) {
+                        var trackName = track.name;
+                        overlayHTML += '<li class="album-track">' + trackName + '</li>';
+                    });
+                    // Closing the HTML
+                    overlayHTML += '</ul>';
+                    overlayHTML += '</section>';
+                    overlayHTML += '</div>';
+                    overlayHTML += '</div>';
 
-            $.each(tracks, function(i, track) {
-                var trackName = track.name;
-                // Add each track track list
-                trackList += trackName;
+                    // Inject the HTML into the DOM (on top)
+                    $('body').prepend(overlayHTML);
+                    $(overlay).hide();
+                    $(overlay).fadeIn(400);
+
+                },
+                hideOverlay = function() {
+                    function removeOverlay() {
+                        $(overlay).remove();
+                    }
+                    // Nice transition
+                    $(overlay).fadeOut(400);
+                    // Removing from DOM
+                    setTimeout(removeOverlay, 400);
+                    $(document).off('keydown');
+                };
+            showOverlay();
+            // On click of close button
+            $(closeButton).click(function() {
+                hideOverlay();
             });
-            console.log( /*albumId, artistName, albumName, albumImgUrl, albumReleased, */ trackList);
         }
     });
 }
 
+
+/* --------------------------------------------------------------------------- *\
+    SEARCH
+\* --------------------------------------------------------------------------- */
+
 // Search albums based on keyword
 function searchAlbums() {
     var searchQuery = $input.val();
-
     // Get data 
     $.ajax({
         url: 'https://api.spotify.com/v1/search',
@@ -48,7 +92,6 @@ function searchAlbums() {
             var imageGallery = $('#image-gallery'),
                 albums = response.albums.items,
                 resultHTML = '';
-            console.log(albums);
 
             $.each(albums, function(i, album) {
                 var albumId = album.id,
@@ -61,7 +104,7 @@ function searchAlbums() {
                 resultHTML += '</a>';
                 resultHTML += '</li>';
             });
-            // Inject the HTML into the result list
+            // Inject the HTML into DOM (the result list)
             imageGallery.html(resultHTML);
             //On click of thumbnail
             $("a").click(function(event) {
@@ -78,13 +121,3 @@ $($input).keyup(function() {
     // Immediately search for albums by using the keyword
     searchAlbums();
 });
-
-/* --------------------------------------------------------------------------- *\
-    GALLERY
-\* --------------------------------------------------------------------------- */
-
-
-// If a thumbnail is clicked
-// A lightbox shows up
-// the bigger image is loaded
-// and extra data
